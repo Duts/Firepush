a9257472d44ece293e4365e4e13aaec491111d97
Abyss777 <abyss@fox5.ru>
2015-08-07 18:34:01 +0500

    - Fixed error then "Enter token" is not displayed on first start
    - Added listening WebSocket for pushes updates
    - Added showing notifications on new push

--- js/request.js
+++ js/request.js
@@ -4,7 +4,7 @@
 
 */
 
-if (localStorage.getItem("login") == 0) {
+if (!(localStorage.getItem("login"))) {
 
 //Access
 var token = prompt("Type the your token for login");
@@ -44,86 +44,88 @@ if(localStorage.getItem("login") != 1){
 	}
 }
 
-// WebSocket initialization
-if(localStorage.getItem("login") = 1) {
-	if (websocket != null) {
-        websocket.close();
-    }
-	websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' +localStorage.getItem("token"));
-	websocket.onopen = function(e) { console.log( "WebSocket opened") }
-	websocket.onmessage = function(e) { console.log( "Get WebSocket message " + e.data) }
-    websocket.onerror = function(e) { console.log( "WebSocket error") }
-    websocket.onclose = function(e) { console.log( "WebSocket closed") }
-}
-
-//Load all pushs
-var push_request = new XMLHttpRequest();
-push_request.open("GET","https://api.pushbullet.com/v2/pushes",true);
-push_request.setRequestHeader("Authorization","Bearer "+localStorage.getItem("token"));
-push_request.send();
-console.log(push_request);
-
 //Contain all iden
 var iden = [];
 
-push_request.onreadystatechange=function()
-{
-	if (push_request.readyState == 4 && push_request.status == 200) {
-		var parse_push = JSON.parse(push_request.responseText);
-		for (var i = 0; i < parse_push.pushes.length; i++) {
-		 	var kid = document.createElement("div");
-		 	kid.setAttribute("class","push_container_style");
-		    var type = parse_push.pushes[i].type;
-		    iden[i] = parse_push.pushes[i].iden;
+var updatePushes = function updatePushes(last_modified) {
+	var push_request = new XMLHttpRequest();
+	push_request.open("GET","https://api.pushbullet.com/v2/pushes?modified_after="+ last_modified,true);
+	push_request.setRequestHeader("Authorization","Bearer "+localStorage.getItem("token"));
+	push_request.send();
+	console.log(push_request);
+	
+	il = iden.length
 
-			if (type == "note") {
-				if (parse_push.pushes[i].title == undefined || parse_push.pushes[i].title == null) {
-					kid.innerHTML = "<img src='images/note_image.png' class='im' />"+"<p>"+parse_push.pushes[i].body+"</p>";
-				}else{
-					kid.innerHTML = "<img src='images/note_image.png' class='im' /><span>&nbsp;"+parse_push.pushes[i].title+"</span><p>"+parse_push.pushes[i].body+"</p>";
-				}
-			}else if (type == "link") {
-				if (parse_push.pushes[i].body == undefined || parse_push.pushes[i].body == null) {
-					var body_link =	"<p><a href='"+parse_push.pushes[i].url+"'>"+parse_push.pushes[i].url+"</a></p>";
-				}else{
-					var body_link =	"<p>"+parse_push.pushes[i].body+"<br /><a href='"+parse_push.pushes[i].url+"'>"+parse_push.pushes[i].url+"</a></p>";
-				}
-				if (parse_push.pushes[i].title == undefined || parse_push.pushes[i].title == null) {
-					kid.innerHTML = "<img src='images/link_image.png' class='im' />"+body_link;
-				}else{
-					kid.innerHTML = "<img src='images/link_image.png' class='im' /><span>&nbsp;"+parse_push.pushes[i].title+"</span>"+body_link;
+	push_request.onreadystatechange=function()
+	{
+		if (push_request.readyState == 4 && push_request.status == 200) {
+			var parse_push = JSON.parse(push_request.responseText);
+			console.log(push_request);
+			var FirstPush = document.getElementById("push_container").firstChild
+			for (var i = 0; i < parse_push.pushes.length; i++) {
+				var newPush = false;
+				if (document.getElementById(parse_push.pushes[i].iden)) { var kid = document.getElementById(parse_push.pushes[i].iden) }
+					else { var kid = document.createElement("div"); kid.id = parse_push.pushes[i].iden;  newPush = true}
+				kid.setAttribute("class","push_container_style");
+				var type = parse_push.pushes[i].type;
+				iden[il+i] = parse_push.pushes[i].iden;
+				showNotification = (parse_push.pushes[i].active && !(parse_push.pushes[i].dismissed))
+				if (type == "note") {
+					if (parse_push.pushes[i].title == undefined || parse_push.pushes[i].title == null) {
+						kid.innerHTML = "<img src='images/note_image.png' class='im' />"+"<p>"+parse_push.pushes[i].body+"</p>";
+						if (showNotification) {createNotification("",parse_push.pushes[i].body,il+i);}
+					}else{
+						kid.innerHTML = "<img src='images/note_image.png' class='im' /><span>&nbsp;"+parse_push.pushes[i].title+"</span><p>"+parse_push.pushes[i].body+"</p>";
+						if (showNotification) {createNotification(parse_push.pushes[i].title,parse_push.pushes[i].body,il+i);}
+					}
+				}else if (type == "link") {
+					if (parse_push.pushes[i].body == undefined || parse_push.pushes[i].body == null) {
+						var body_link =	"<p><a href='"+parse_push.pushes[i].url+"'>"+parse_push.pushes[i].url+"</a></p>";
+					}else{
+						var body_link =	"<p>"+parse_push.pushes[i].body+"<br /><a href='"+parse_push.pushes[i].url+"'>"+parse_push.pushes[i].url+"</a></p>";
+					}
+					if (parse_push.pushes[i].title == undefined || parse_push.pushes[i].title == null) {
+						kid.innerHTML = "<img src='images/link_image.png' class='im' />"+body_link;
+						if (showNotification) {createNotification("",body_link,il+i);}
+					}else{
+						kid.innerHTML = "<img src='images/link_image.png' class='im' /><span>&nbsp;"+parse_push.pushes[i].title+"</span>"+body_link;
+						if (showNotification) {createNotification(parse_push.pushes[i].title,body_link,il+i);}
+					}
+
+				}else if (type == "address"){
+					kid.innerHTML = "<img src='images/address_image.png' class='im' /><span>&nbsp; Address: "+ parse_push.pushes[i].name +"</span>";
+
+				}else if (type == "file") {
+					if (parse_push.pushes[i].file_type == "image/png" || parse_push.pushes[i].file_type == "image/jpg" || parse_push.pushes[i].file_type == "image/jpeg" || parse_push.pushes[i].file_type == "image/gif" || parse_push.pushes[i].file_type == "image/bpm") {
+						kid.innerHTML = "<img src='" + localStorage.getItem("avatar") + "' class='im' /><p><a href='" + parse_push.pushes[i].file_url + "' ><img src='" + parse_push.pushes[i].file_url + "' class='im_push'></a></p>";
+					}else{
+						kid.innerHTML = "<img src='" + localStorage.getItem("avatar") + "' class='im' /><p><a href='" + parse_push.pushes[i].file_url + "' >" + parse_push.pushes[i].file_name + "</a></p>";
+					}
 				}
 
-			}else if (type == "address"){
-				kid.innerHTML = "<img src='images/address_image.png' class='im' /><span>&nbsp; Address: "+ parse_push.pushes[i].name +"</span>";
-
-			}else if (type == "file") {
-				if (parse_push.pushes[i].file_type == "image/png" || parse_push.pushes[i].file_type == "image/jpg" || parse_push.pushes[i].file_type == "image/jpeg" || parse_push.pushes[i].file_type == "image/gif" || parse_push.pushes[i].file_type == "image/bpm") {
-					kid.innerHTML = "<img src='" + localStorage.getItem("avatar") + "' class='im' /><p><a href='" + parse_push.pushes[i].file_url + "' ><img src='" + parse_push.pushes[i].file_url + "' class='im_push'></a></p>";
-				}else{
-					kid.innerHTML = "<img src='" + localStorage.getItem("avatar") + "' class='im' /><p><a href='" + parse_push.pushes[i].file_url + "' >" + parse_push.pushes[i].file_name + "</a></p>";
+				//Check if a push is active or not
+				if (parse_push.pushes[i].active == true) {
+					var trash = document.createElement("img");
+					trash.setAttribute("src","images/trash.png");
+					trash.setAttribute("class","trash");
+					trash.setAttribute("title",i);
+					trash.setAttribute("onclick","del("+i+")");
+					document.getElementById("push_container").appendChild(trash);
 				}
+				if (parse_push.pushes[i].modified) {
+					if (localStorage.getItem("last_modified") < parse_push.pushes[i].modified) { localStorage.setItem("last_modified",parse_push.pushes[i].modified); }
+					}
+
+				if (newPush) {document.getElementById("push_container").insertBefore(kid, FirstPush);}
 			}
-
-			//Check if a push is active or not
-			if (parse_push.pushes[i].active == true) {
-			 	var trash = document.createElement("img");
-			 	trash.setAttribute("src","images/trash.png");
-			 	trash.setAttribute("class","trash");
-			 	trash.setAttribute("title",i);
-			 	trash.setAttribute("onclick","del("+i+")");
-				document.getElementById("push_container").appendChild(trash);
-		 	}
-
-			document.getElementById("push_container").appendChild(kid);
+		}else if(push_request.status == 400 || push_request.status == 401 || push_request.status == 403 || push_request.status == 404){
+			alert(parse_push.error.message);
 		}
-	}else if(push_request.status == 400 || push_request.status == 401 || push_request.status == 403 || push_request.status == 404){
-		alert(parse_push.error.message);
 	}
+
 }
 
-
-
+updatePushes(0)
 
 
 //Set value for hide/show menu
@@ -188,6 +190,19 @@ function del(a) {
 	}
 }
 
+function dismiss(a) {
+	console.log("Dismissing "+ a + " iden:"+iden[a] );
+	var dis = new XMLHttpRequest();
+	dis.open("POST","https://api.pushbullet.com/v2/pushes/"+iden[a],false);
+	dis.setRequestHeader("Authorization","Bearer "+localStorage.getItem("token"));
+	dis.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	var blob = '{"dismissed": true}';
+	dis.send(blob);
+	console.log(dis);
+}
+
+
+
 function logout () {
 	if (websocket != null) {
         websocket.close();
@@ -195,4 +210,43 @@ function logout () {
 	localStorage.clear();
 	localStorage.setItem("login","0");
 	location.reload();
+}
+
+// Creates a notification using our app icon and adding some actions
+  var createNotification = function createNotification(title,body,pos) {
+	var options = {
+      		body: body,
+      		icon: "images/icon64.png"
+  			}
+	console.log('Creating notification');
+    var n = new Notification(title,options)
+	// Handle notification clicks
+	n.pos = pos
+    n.onclick = function () {
+		console.log('Notification clicked');
+		navigator.mozApps.getSelf().onsuccess = function(e) { e.target.result.launch();	}
+		dismiss(this.pos);
+        this.close();
+	    }
+    console.log('Notification created');
+    }
+
+
+// WebSocket initialization
+if(localStorage.getItem("login") == 1) {
+	//if (websocket != null) {
+    //    websocket.close();
+    //}
+	websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' +localStorage.getItem("token"));
+	websocket.onopen = function(e) { console.log( "WebSocket opened") }
+	websocket.onmessage = function(e) {
+		console.log( "Get WebSocket message " + e.data);
+		var message = JSON.parse(e.data);
+		if (message.type  == "tickle" &&  message.subtype == "push") {
+		console.log( "Something is updated")
+		updatePushes(localStorage.getItem("last_modified"));
+        	}
+	}
+    websocket.onerror = function(e) { console.log( "WebSocket error") }
+    websocket.onclose = function(e) { console.log( "WebSocket closed") }
 }
