856cbf4753791fe2a2eb20bdbff765007d57e74b
Robert Halandut <Duts@users.noreply.github.com>
2015-09-23 18:24:35 +0200

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2015 Robert Haladut, Andrey Kunitsyn
+Copyright (c) 2015 Robert Halandut, Andrey Kunitsyn
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
